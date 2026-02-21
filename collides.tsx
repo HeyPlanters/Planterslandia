c39eb1fa94e60bcec2912696fec35cf415346d06
HeyPlanters <heyplanters@gmail.com>
2026-02-22 00:18:51 +0100

    Delete collides.tsx

--- collides.tsx
+++
@@ -1,9 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="collides" tilewidth="32" tileheight="32" tilecount="1" columns="1">
- <image source="collides.png" width="32" height="32"/>
- <tile id="0">
-  <properties>
-   <property name="collides" type="bool" value="true"/>
-  </properties>
- </tile>
-</tileset>
